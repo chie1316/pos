f73229fdb5467f1a730f2534acc35dca9b575ee9
chie1316 <aramirez1316@gmail.com>
2022-03-04 14:18:10 +0800

    Initial commit

---
+++ src/test/java/com/ramirez/pos/PosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramirez.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
